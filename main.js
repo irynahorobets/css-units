f2e38f574386d1e15a4112f52f2acc8ab080a074
irynahorobets <iryna.horobets@brightlocal.com>
2024-12-17 13:54:37 +0100

    initial em example

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 (function() {
-    const units = ['px', 'rem'];
-    const elements = Array.from(document.getElementsByClassName("unit-title"));
-    elements.forEach((element, index) => {
-        element.innerHTML = units[index] + ' example';
-    });
-    const textElements = Array.from(document.getElementsByClassName("example-text"));
-    textElements.forEach((element, index) => {
-        element.innerHTML = 'Try adjusting browser default font-size to see how the text size changes.';
-    });
+    
 })();
